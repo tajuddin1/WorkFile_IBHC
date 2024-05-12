3777f91edf55a5455164c64d4413019e9566b357
taj1uddin <itstaj7272@gmail.com>
2024-05-12 17:52:20 +0600

    Update As Client Need

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,21 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-    var menu = document.getElementById("mobile_menu");
-    menu.classList.toggle("extend");
+var hamburger_icon = document.getElementById("hamburger_icon");
+var menu = document.getElementById("mobile_menu");
+
+// Toggle classes when hamburger icon is clicked
+hamburger_icon.onclick = function() {
+    myFunction();
 };
+
+// Function to toggle classes
+function myFunction() {
+    hamburger_icon.classList.toggle("change");
+    menu.classList.toggle("extend");
+}
+
+// Remove classes when clicking outside the menu and hamburger icon
+document.addEventListener("click", function(event) {
+    if (!menu.contains(event.target) && event.target !== hamburger_icon) {
+        menu.classList.remove("extend");
+        hamburger_icon.classList.remove("change");
+    }
+});
